c329a933f9a05770ee338b25e58d475f3108d532
Liu Wenjie <437443756@qq.com>
2022-12-09 09:03:05 +0000

    Add：上传图像预处理文件

---
+++ 1_算法示例/network_files/image_list.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+from torch import Tensor
+
+
+class ImageList(object):
+    """
+    将图像填充为相同的大小，并将每张图像的原始大小存储在一个字段中
+    """
+
+    def __init__(self, tensors, image_sizes):
+        # type: (Tensor, List[Tuple[int, int]]) -> None
+        """
+        Arguments:
+            tensors (tensor) padding后的图像数据
+            image_sizes (list[tuple[int, int]])  padding前的图像尺寸
+        """
+        self.tensors = tensors
+        self.image_sizes = image_sizes
+
+    def to(self, device):
+        # type: (Device) -> ImageList # noqa
+        cast_tensor = self.tensors.to(device)
+        return ImageList(cast_tensor, self.image_sizes)
+
